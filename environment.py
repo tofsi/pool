c9451f89506afe3fd3ef2189d76f0495c75e59dc
HedwigNordlinder <160422581+HedwigNordlinder@users.noreply.github.com>
2025-03-08 15:26:48 +0100

    Gym environment working

--- environment.py
+++ environment.py
@@ -7,20 +7,31 @@ from play_pool import simulate
 # Our observation space is [array of positions] + [pocketed balls]
 class PoolEnvironment(gym.Env):
 
-    ball_positions = [] # Array of (x,y) positions
-    pocketed = [] # Categorical indicators for if balls are pocketed
-    ball_count = 0 # Count of balls, includes white ball
-    
-    initial_ball_positions = []
-    
-    action_space = gym.spaces.Box(low = [0,0], high = [1, 2*np.pi],shape=(2,), dtype = np.float32)
-    # Observation space is 
-    position_space = gym.spaces.Box(low = [0,0], high = [2,1], shape = (ball_count,2), dtype = np.float32)
-    pocketed_space = gym.spaces.Box(low = 0, high = 1, shape =(ball_count,),dtype=np.int32)
-    observation_space = gym.spaces.Dict(positions = position_space, pocketed = pocketed_space)
+    metadata = {"render_modes": ["human", "rgb_array"]}
 
-
-    def __init__(self):
+    def __init__(self, ball_count = 2):
+                # Instance attributes
+        self.ball_count = ball_count
+        self.balls = []  # Array of (x,y) positions
+        self.pocketed = np.zeros(ball_count)  # Categorical indicators for balls pocketed
+        self.initial_ball_positions = []
+        
+        # Define action space - angle and force
+        self.action_space = gym.spaces.Box(
+            low=np.array([0, 0]), 
+            high=np.array([1, 2*np.pi]), 
+            shape = (2,),
+            dtype=np.float32
+        )
+        
+        
+        self.observation_space = gym.spaces.Box(
+            low=-np.inf, 
+            high=np.inf, 
+            shape=(3 * ball_count,), 
+            dtype=np.float32
+        )   
+        
         print("Initialised")
 
     def step(self, action): # An action is a tuple of (angle, share_of_max_force). 
@@ -37,7 +48,7 @@ class PoolEnvironment(gym.Env):
         pocketed = pocketed + newly_pocketed
         self.ball_positions = new_ball_positions
         white_ball_pocketed = newly_pocketed[0] == 1 # This is a terminal state
-        all_non_white_balls_pocketed = sum(pocketed) == self.ball_count - 1 and not white_ball_pocketed
+        all_non_white_balls_pocketed = np.sum(pocketed) == self.ball_count - 1 and not white_ball_pocketed
 
 
         if white_ball_pocketed:
@@ -45,14 +56,14 @@ class PoolEnvironment(gym.Env):
             terminated = True
             observation = (self.balls, self.pocketed)
         else:
-            reward = sum(newly_pocketed) / collisions
+            reward = np.sum(newly_pocketed) / collisions
             terminated = all_non_white_balls_pocketed
             observation = (self.balls, self.pocketed)
         
         return observation, reward, terminated, truncated, info
 
     def reset(self):
-        self.ball_positions = self.initial_ball_positions.copy()
+        self.balls = self.initial_ball_positions.copy()
         self.pocketed = np.zeros(self.ball_count)
 
 
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+from environment import PoolEnvironment
+import sys
+sys.path.append('/home/hedwig/Documents/Python/spinningup_tf2')
+import spinup_bis as spinup
 if __name__ == "__main__":
-    print("Hello World")
+    def environment_function(): return PoolEnvironment()
+    spinup.ddpg_tf2(environment_function)
